bfce8f7f70bbbefacaba0ab38519fb8fe0e0b6a0
Alex Renoki <rennokki@gmail.com>
2021-11-08 14:26:57 +0200

    Closes #5

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CheckSessionController;
+use App\Http\Controllers\EditUserController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
@@ -21,6 +22,7 @@ Route::post('/users/register', RegisterController::class)->name('register');
 Route::post('/users/login', LoginController::class)->name('login');
 Route::post('/users/logout', LogoutController::class)->name('logout');
 Route::post('/users/checkSession', CheckSessionController::class)->name('check-session');
+Route::any('/users/edit', EditUserController::class)->name('edit-user');
 
 Route::middleware(['auth:api'])->group(function () {
     //
